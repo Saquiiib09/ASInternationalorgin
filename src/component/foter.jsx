9724f52fcf73b08c493732341bbd4c1e428c8907
Saquib khan <saquibkhan15750@gmail.com>
2025-07-22 13:26:58 +0530

    Update foter.jsx

--- src/component/foter.jsx
+++ src/component/foter.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { Link } from "react-router-dom";
 import "../css/footer.css";
-import logo from '../assets/images/logo.png';
+// import logo from '../assets/images/logo.png';
 
 const Footer = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -23,24 +23,24 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer-section">
         <div className="company-info">
-          <img src={logo} alt="AS International" className="footer-logo" />
+          {/* <img src={logo} alt="AS International" className="footer-logo" />
           <p>
             Offering trusted and critical advice to your HR-related issues and
             queries, however complex they may be.
-          </p>
+          </p> */}
           <div className="social-icons">
-            <Link to="#" className="iconn"><i className="fab fa-facebook"></i></Link>
-            <Link to="#" className="iconn"><i className="fab fa-twitter"></i></Link>
-            <Link to="#" className="iconn"><i className="fab fa-google"></i></Link>
-            <Link to="#" className="iconn"><i className="fab fa-instagram"></i></Link>
+            {/* <Link to="#" className="iconn"><i className="fab fa-facebook"></i></Link>
+            <Link to="#" className="iconn"><i className="fab fa-twitter"></i></Link> */}
+            <Link to="https://g.co/kgs/VLGA3kP" className="iconn"><i className="fab fa-google"></i></Link>
+            <Link to="https://www.linkedin.com/company/a-s-international-hr-consultancy" className="iconn"><i className="fab fa-linkedin"></i></Link>
           </div>
         </div>
-        <div className="contact-info">
+        {/* <div className="contact-info">
           <h3>Contact Us</h3>
           <p><span className="iconm">📍</span><strong>Address:</strong> SAMNATH, Samantbhai Nanji Marg, near children jail, Dongri, Mumbai, Maharashtra 400009</p>
           <p><span className="iconm">📧</span><strong>Email Us:</strong> <Link className="footer_email" to="mailto:admin@asinternational.org.in">admin@asinternational.org.in</Link></p>
           <p><Link to="https://wa.me/919769456956" target="_blank" className="phone_footer" rel="noopener noreferrer"><span className="iconm">📞</span><strong>Phone:</strong> +91 9769456956</Link></p>
-        </div>
+        </div> */}
         <div className="quick-links">
           <h3>Quick Links</h3>
           <ul>
